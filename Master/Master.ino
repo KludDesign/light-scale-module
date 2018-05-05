b2ccbcfd006449b33dc85e6db409067340ae7396
KludDesign <vincentgillibert@gmail.com>
2018-05-06 00:28:33 +0200

    Enlever commentaire V1

--- Master/Master.ino
+++ Master/Master.ino
@@ -12,7 +12,7 @@ const int latchPin = 8;  // Pin connected to latch pin (ST_CP) of 74HC595
 const int clockPin = 12; // Pin connected to clock pin (SH_CP) of 74HC595
 const int dataPin = 11;  // Pin connected to Data in (DS) of 74HC595
 
-const byte col = 9; // Column number of the matrix
+const byte col = 9; 
 
 // Matrix to swich on LEDS
 unsigned int sequence[][col] = {
@@ -94,24 +94,24 @@ unsigned int sequence[][col] = {
     B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111};
 
 void setup() {
-  
+
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
-  
+
   pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);  
+  pinMode(dataPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
 }
 
 void loop() {
-  
+
     Wire.requestFrom(1, 1);        // request 6 bytes from slave device #1
     while (Wire.available()) {     // slave may send less than requested
       ValKgEsclave1 = Wire.read(); // Get the value from slave 1
     }
 
     delay(500);
-    
+
     Wire.requestFrom(2, 1);        // request 6 bytes from slave device #2
     while (Wire.available()) {     // slave may send less than requested
       ValKgEsclave2 = Wire.read(); // Get the value from slave 2
@@ -136,7 +136,7 @@ void loop() {
     //Serial.println(ValKgEsclave2, DEC);
     Serial.println(ValKgEsclave3, DEC);
     //Serial.println("");
-   
+
     // If weight >100kg
     if (ValKgEsclave1 > 110 || ValKgEsclave2 > 110 || ValKgEsclave3 > 110){
             digitalWrite(latchPin, LOW);
@@ -150,7 +150,7 @@ void loop() {
             shiftOut(dataPin, clockPin, MSBFIRST, sequence[73][7]);
             shiftOut(dataPin, clockPin, MSBFIRST, sequence[73][8]);
             digitalWrite(latchPin, HIGH);}
-          
+
     else{
           // If 2 people get on two diffrent scales at the same time
           if (ValKgEsclave1>15 && ValKgEsclave2>15 || ValKgEsclave1>15 && ValKgEsclave3>15 || ValKgEsclave2>15 && ValKgEsclave3>15){
@@ -180,9 +180,9 @@ void loop() {
 
           // If no body on any scale
           else{
-            
+
             if (ValKgEsclave1<5 && ValKgEsclave2<5 && ValKgEsclave3<5){
-          
+
               digitalWrite(latchPin, LOW);
               shiftOut(dataPin, clockPin, MSBFIRST, sequence[1][0]);
               shiftOut(dataPin, clockPin, MSBFIRST, sequence[1][1]);
@@ -199,9 +199,9 @@ void loop() {
               else{
                 // Child scale
                 if (ValKgEsclave1>ValKgEsclave2 && ValKgEsclave1>ValKgEsclave3){
-                  
+
                     ValKgEnfant = (ValKgEsclave1)*0.68; // Water quantity  = 70%
-              
+
                     for (int row = 0; row < ValKgEnfant; row++){
                         digitalWrite(latchPin, LOW);
                         shiftOut(dataPin, clockPin, MSBFIRST, sequence[row][0]);
@@ -214,14 +214,14 @@ void loop() {
                         shiftOut(dataPin, clockPin, MSBFIRST, sequence[row][7]);
                         shiftOut(dataPin, clockPin, MSBFIRST, sequence[row][8]);
                         digitalWrite(latchPin, HIGH);}}
-            
+
                 delay(300);
-            
+
                 // Adult scale
                 if (ValKgEsclave2>ValKgEsclave1 && ValKgEsclave2>ValKgEsclave3){
-                  
+
                     ValKgAdulte = (ValKgEsclave2)*0.65; // Water quantity  = 62%
-                    
+
                     for (int row = 0; row < ValKgAdulte; row++){
                         digitalWrite(latchPin, LOW);
                         shiftOut(dataPin, clockPin, MSBFIRST, sequence[row][0]);
@@ -234,14 +234,14 @@ void loop() {
                         shiftOut(dataPin, clockPin, MSBFIRST, sequence[row][7]);
                         shiftOut(dataPin, clockPin, MSBFIRST, sequence[row][8]);
                         digitalWrite(latchPin, HIGH);}}
-          
+
                 delay(300);
-              
+
                 // Old person scale
                 if (ValKgEsclave3>ValKgEsclave1 && ValKgEsclave3>ValKgEsclave2){
-                  
+
                     ValKgAgee = (ValKgEsclave3)*0.55; // Water quantity = 55%
-                    
+
                     for (int row = 0; row < ValKgAgee; row++){
                         digitalWrite(latchPin, LOW);
                         shiftOut(dataPin, clockPin, MSBFIRST, sequence[row][0]);
@@ -264,4 +264,3 @@ void loop() {
   }
   delay(300);
 }
-
